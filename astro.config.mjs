e1f260798e75a58b576c1fd9395d2703272c035a
casoon <joern.seidel@casoon.de>
2024-09-14 12:31:21 +0200

    config adjusted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import alpine from '@astrojs/alpinejs';
+import sitemap from '@astrojs/sitemap';
+import config from './src/config'
 
 export default defineConfig({
-    integrations: [tailwind(), alpine()],
+    integrations: [tailwind(), alpine(), sitemap({
+        filter: (page) =>
+            page !== config.siteUrl + '/test/' &&
+            page !== config.siteUrl + '/test1/',
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date(),
+    })],
+    server: {
+        port: 3000
+    },
 });
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export default {
+    author: "CASOON",
+    siteUrl: "https://bun-astro-starter.casoon.dev",
+    excludeFromRobots: ['/private', '/admin'] // Seiten, die du von Robots ausschließen möchtest
+}
